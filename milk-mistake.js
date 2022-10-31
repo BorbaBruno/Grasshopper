2411a30b755782db47c98ee96f380eece75485cf
BorbaBruno <brunodeborba@outlook.com>
2022-10-31 15:11:06 -0300

    doing good

---
+++ milk-mistake.js
@@ -0,0 +1,17 @@
+
+let cart = [
+
+    'flour',
+    'milk',
+    'milk',
+    'eggs',
+]
+
+console.log(cart);
+
+let index = cart.lastIndexOf('milk');
+
+console.log('The extra item is at index: ' + index);
+
+// indexOf(Will return the first index where 'milk' is in the array)
+// lastIndexOf(Will return the last index where 'milk' is in the array)
